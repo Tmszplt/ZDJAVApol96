57cce15c2f44579d56387ae6b549917036286da6
TomaszP <tmszpilat@gmail.com>
2021-07-19 18:40:08 +0200

    Pierwsza wersja programu\

---
+++ src/main/java/Projekt/Projekt1.java
@@ -0,0 +1,9 @@
+package Projekt;
+
+public class Projekt1 {
+    public static void main(String[] args) {
+        double procentLokaty;
+        double kwotaLokaty;
+        int liczbaLat;
+    }
+}
